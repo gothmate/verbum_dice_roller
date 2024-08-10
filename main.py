5e78d338c9f7c222c2d6ceab00ce785bd6ed2ed7
gothmate <gothmate@gmail.com>
2024-08-10 17:52:22 -0300

    Feature(funções): - criadas funções de rolamentos e dificuldades.

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
 import random
-
 from CONSTANTS import *
+
+# Defina URLs das imagens para cada resultado
+IMAGES = {
+    "natural": "https://raw.githubusercontent.com/gothmate/verbum_dice_roller/dev/images/d8.jpg",
+    "social": "https://raw.githubusercontent.com/gothmate/verbum_dice_roller/dev/images/d8.jpg",
+    "racional": "https://raw.githubusercontent.com/gothmate/verbum_dice_roller/dev/images/d8.jpg"
+}
 
 intents = nextcord.Intents.default()
 intents.members = True
@@ -16,57 +22,146 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     print("-----------------------------------------")
 
-testingServerID = 1270162128992600155
+ServerID = 1270162128992600155
 
-@client.slash_command(name='hello', description="Diz Olá", guild_ids=[testingServerID])
+@client.slash_command(name='hello', description="Diz Olá", guild_ids=[ServerID])
 async def hellocommand(interaction: Interaction):
     await interaction.response.send_message(f'Olá.')
 
-@client.slash_command(name='roll', description="rola 3 dados de 8 lados.", guild_ids=[testingServerID])
+@client.slash_command(name='roll-nr4', description="rola 3 dados de 8 lados.", guild_ids=[ServerID])
 async def rollcommand(interaction: Interaction):
-  firstDie = random.randint(1, 8)
-  secondDie = random.randint(1, 8)
-  thirdDie = random.randint(1, 8)
-  
-  if firstDie == 4 or firstDie == 8:
-    sucessOne = "sucesso!"
-  else:
-    sucessOne = "falha!"
-    
-  if secondDie == 4 or secondDie == 8:
-    sucessTwo = "sucesso!"
-  else:
-    sucessTwo = "falha!"
-    
-  if thirdDie == 4 or thirdDie == 8:
-    sucessThree = "sucesso!"
-  else:
-    sucessThree = "falha!"
-  
-  diceList = {
-    "firstpool": [firstDie, sucessOne],
-    "secondpool": [secondDie, sucessTwo],
-    "thirdpool": [thirdDie, sucessThree],
-  }
-  
-  numero_de_sucessos = contar_sucessos(diceList)
-  if numero_de_sucessos > 1: 
-    resultado = "Maravilha!!!" 
-  elif numero_de_sucessos == 1:
-    resultado = "Foi bem."
-  else:
-    resultado = "Falhou miseravelmente."
+    firstDie = random.randint(1, 8)
+    secondDie = random.randint(1, 8)
+    thirdDie = random.randint(1, 8)
 
-  
-  await interaction.response.send_message(
-    f'Dado 1: {diceList["firstpool"][0]} {diceList["firstpool"][1]}\n'
-    f'Dado 2: {diceList["secondpool"][0]} {diceList["secondpool"][1]}\n'
-    f'Dado 3: {diceList["thirdpool"][0]} {diceList["thirdpool"][1]}\n\n'
-    f'{numero_de_sucessos} sucessos!\n'
-    f'{resultado}'
-  )
-  
+    if firstDie == 4 or firstDie == 8:
+      sucessOne = True 
+    else: 
+      sucessOne = False
+    if secondDie == 4 or secondDie == 8:
+      sucessTwo = True 
+    else: 
+      sucessTwo = False
+    if thirdDie == 4 or thirdDie == 8:
+      sucessThree = True 
+    else: 
+      sucessThree = False
+
+    diceList = {
+        "natural": [firstDie, sucessOne],
+        "social": [secondDie, sucessTwo],
+        "racional": [thirdDie, sucessThree],
+    }
+
+    numero_de_sucessos = contar_sucessos(diceList)
+    if numero_de_sucessos > 1:
+        resultado = "Maravilha!!!"
+    elif numero_de_sucessos == 1:
+        resultado = "Foi bem."
+    else:
+        resultado = "Falhou miseravelmente."
+
+    # Criando os embeds com imagens diferentes
+    embeds = []
+    for key, value in diceList.items():
+        embed = nextcord.Embed(
+            title=f"Resultado {key}",
+            description=f"{key}: {value[0]} - {"Sucesso" if value[1] == True else "Falha"}",
+            color=0x00ff00 if value[1] == True else 0xff0000
+        )
+        embed.set_image(url=IMAGES[key])
+        embeds.append(embed)
+
+    # Enviando os embeds
+    for embed in embeds:
+        await interaction.channel.send(embed=embed)
+    
+    # Enviando a mensagem final com o resultado
+    await interaction.channel.send(
+        f'{numero_de_sucessos} sucessos!\n{resultado}'
+    )
+    
+@client.slash_command(name='roll-nr7', description="rola 3 dados de 8 lados.", guild_ids=[ServerID])
+async def rollcommand(interaction: Interaction):
+    firstDie = random.randint(1, 8)
+    secondDie = random.randint(1, 8)
+    thirdDie = random.randint(1, 8)
+
+    if firstDie == 7 or firstDie == 8:
+      sucessOne = True 
+    else: 
+      sucessOne = False
+    if secondDie == 7 or secondDie == 8:
+      sucessTwo = True 
+    else: 
+      sucessTwo = False
+    if thirdDie == 7 or thirdDie == 8:
+      sucessThree = True 
+    else: 
+      sucessThree = False
+
+    diceList = {
+        "natural": [firstDie, sucessOne],
+        "social": [secondDie, sucessTwo],
+        "racional": [thirdDie, sucessThree],
+    }
+
+    numero_de_sucessos = contar_sucessos(diceList)
+    if numero_de_sucessos > 1:
+        resultado = "Maravilha!!!"
+    elif numero_de_sucessos == 1:
+        resultado = "Foi bem."
+    else:
+        resultado = "Falhou miseravelmente."
+
+    # Criando os embeds com imagens diferentes
+    embeds = []
+    for key, value in diceList.items():
+        embed = nextcord.Embed(
+            title=f"Resultado {key}",
+            description=f"{key}: {value[0]} - {"Sucesso" if value[1] == True else "Falha"}",
+            color=0x00ff00 if value[1] == True else 0xff0000
+        )
+        embed.set_image(url=IMAGES[key])
+        embeds.append(embed)
+
+    # Enviando os embeds
+    for embed in embeds:
+        await interaction.channel.send(embed=embed)
+    
+    # Enviando a mensagem final com o resultado
+    await interaction.channel.send(
+        f'{numero_de_sucessos} sucessos!\n{resultado}'
+    )
+    
+@client.slash_command(name='roll-one', description="rola 1 dado de 8 lados.", guild_ids=[ServerID])
+async def rollcommand(interaction: Interaction):
+    roll = random.randint(1, 8)
+
+    if roll == 4 or roll == 8:
+      sucess = True 
+    else: 
+      sucess = False
+    
+    
+    msg = ''
+    if sucess == True:
+        msg="Sucesso"
+    else:
+        msg="Falha"
+        
+    embed = nextcord.Embed(
+        title=f"Resultado {roll}",
+        description=f"O rolamento do dado resultou em {roll} - {msg}",
+        color=0x00ff00 if sucess == True else 0xff0000
+    )
+    embed.set_image(url='https://raw.githubusercontent.com/gothmate/verbum_dice_roller/dev/images/d8.jpg')
+
+    # Enviando os embeds
+    await interaction.channel.send(embed=embed)
+    
+
 def contar_sucessos(dice_list):
-    return sum(1 for result in dice_list.values() if result[1] == "sucesso!")
-  
+    return sum(1 for result in dice_list.values() if result[1] == True)
+
 client.run(BOT_TOKEN)
